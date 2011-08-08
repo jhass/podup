75301d01a172c79f594684cb143a625f38b1470c
MrZYX <pr0fkill@gmail.com>
2011-08-09 00:31:37 +0200

    copy config before database action

--- script/setup_travis.sh
+++ script/setup_travis.sh
@@ -2,14 +2,14 @@
 echo "copy config/database.yml.example to config/database.yml"
 cp config/database.yml.example config/database.yml
 
+echo "copy config/settings.yml.example to config/settings.yml"
+cp config/settings.yml.example config/settings.yml
+
+echo "fetch geoip database"
+./script/get_geoip.sh
+
 echo "create test database"
 mysql -e 'create database podup_test;' >/dev/null
 
 echo "load database"
 RAILS_ENV=test bundle exec rake db:schema:load
-
-echo "fetch geoip database"
-./script/get_geoip.sh
-
-echo "copy config/settings.yml.example to config/settings.yml"
-cp config/settings.yml.example config/settings.yml
