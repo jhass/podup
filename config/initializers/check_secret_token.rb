4b648957237abd6707879603ce122b222c6a12bf
MrZYX <pr0fkill@gmail.com>
2011-08-09 02:26:19 +0200

    Revert "generate secret token if it doesn't exists"
    This reverts commit 89e5da5337b34dbf91cf2898e255a2af3280c652.

--- config/initializers/check_secret_token.rb
+++
@@ -1,5 +0,0 @@
-# from https://github.com/diaspora/diaspora/blob/master/config/initializers/check_session_secret.rb
-unless File.exists?( File.join(Rails.root, 'config', 'initializers', 'secret_token.rb'))
-  `rake generate:secret_token`
-   require  File.join(Rails.root, 'config', 'initializers', 'secret_token.rb')
-end
