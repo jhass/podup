f2a1d82f38dbcb725ddeae0461eb72a04c2e6634
MrZYX <pr0fkill@gmail.com>
2011-08-09 03:00:32 +0200

    don't download geoip db on ci, lets stub it later if we spec that stuff

--- script/setup_travis.sh
+++ script/setup_travis.sh
@@ -5,8 +5,8 @@ cp config/database.yml.example config/database.yml
 echo "copy config/settings.yml.example to config/settings.yml"
 cp config/settings.yml.example config/settings.yml
 
-echo "fetch geoip database"
-./script/get_geoip.sh
+#echo "fetch geoip database"
+#./script/get_geoip.sh
 
 echo "create test database"
 mysql -e 'create database podup_test;' >/dev/null
